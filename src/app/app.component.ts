896527635570026126addfd130356a3d634c7d3a
behemothoz <behemothoz@yandex.ru>
2018-07-19 13:23:17 +0300

    fix update to v.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './shared/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +7,4 @@ import { AuthService } from './shared/auth.service';
 })
 export class AppComponent {
   title = 'app';
-
-  constructor(private _auth: AuthService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ClarityModule } from "@clr/angular";
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { EventsComponent } from './events/events.component';
-import { SpecialEventsComponent } from './special-events/special-events.component';
-
-import { AuthService } from './shared/auth.service';
-import { AuthGuard } from './shared/auth.guard';
-import { EventService } from './shared/event.service';
-import { TokenInterceptorService } from './shared/token-interceptor.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    EventsComponent,
-    SpecialEventsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    ClarityModule
+    BrowserModule
   ],
-  providers: [AuthService, AuthGuard, EventService,
-    { provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptorService,
-      multi: true }
-    ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
